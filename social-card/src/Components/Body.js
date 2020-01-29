71852e46cddfc05d3a01a4aaf354ee690a49cedf
Antquanique Dancy <antdancy95@gmail.com>
2020-01-28 21:19:46 -0500

    added a body component

---
+++ social-card/src/Components/Body.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Body = () =>{
+    return(
+        <div>
+            <div><h3>Body Header</h3></div>
+            <hr/>
+            <div><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ultricies neque, sit amet cursus leo. Sed vitae erat nulla. Vivamus viverra sapien ut suscipit consectetur. Aenean egestas nisl non odio vestibulum suscipit. Maecenas rhoncus sapien metus, et interdum sapien pellentesque quis. Nulla sagittis efficitur odio, sed hendrerit nibh auctor in. Nunc ultrices et nisi id laoreet.</p></div>
+        </div>
+    )
+}
+
+export default Body
